9d9951831cd11f899cb1a87a862ab7d8a45bd7b9
Vũ Quang Thịnh <=>
2016-06-22 18:35:27 +0700

    add Module

---
+++ Module.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vuquangthinh
+ * Date: 6/22/2016
+ * Time: 6:34 PM
+ */
+
+namespace quangthinh\yii\user;
+
+
+class Module extends \yii\base\Module
+{
+}
